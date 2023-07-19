a20ce71ff7c1cf2091b67ea9e4c44ac284a3abc1
csiemichelin <csiemichelin@github.com>
2023-07-19 22:11:24 +0800

    node.js Build NodeJS Backend Server

---
+++ chapter2/server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+    response.end("Hello From NodeJS Server");   //把字串返回到前端
+}); //新增server監聽器去監聽有沒有客戶端發來的請求(request = 請求object, response = 響應object)
+
+const port = 3001;
+const ip = "140.123.97.154";
+
+
+server.listen(port, ip, () => { //call back函數因為沒有參數所以只有一個括號
+    console.log(`Server is running at http://${ip}:${port}`);
+});    //arg1:port, arg2:IP, arg3:call back函數(nodejs server開始監聽時會呼叫這個函數，顯示服務器運行的一些信息)
