baf442d5fa0ac252c7b5248fae02bee98403db88
csiemichelin <csiemichelin@github.com>
2023-10-06 16:19:11 +0800

    10/6 modular

---
+++ Modular design/070-導入文件模組/070-Mcihelin.js
@@ -0,0 +1,1 @@
+module.exports = "我是Michelin YU";
--- Modular design/070-導入文件模組/070-import.js
+++ Modular design/070-導入文件模組/070-import.js
@@ -1,5 +1,20 @@
 //導入模組
-const ImportMoudle = require('./068-moudle.js');
+// fs模組之前學建議用絕對路徑
+// 但require建議用相對路徑，不會受terminal執行的工作目錄影響
+// const ImportMoudle = require('./070-moudle.js');    //不能省略./ or ../
+
+// 可以省略副檔名
+const ImportMoudle = require('./070-moudle');
+
+// 導入JSON文件也可以有相同效果
+// const ImportMichelin = require('./070-Mcihelin.json');
+const ImportMichelin = require('./070-Mcihelin');   //但如果主檔名相同，副檔名不同，則js會比JSON更優先讀取
+console.log(ImportMichelin);
+
+//導入其他類型的文件會視作js文件處理
+// const test = require('./070-Test');
+const test = require('./070-Test.abc');
+console.log(test);
 
 //呼叫函數
 ImportMoudle();
