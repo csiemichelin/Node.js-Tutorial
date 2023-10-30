0a25661645b9f0db78edc75cb2fe3b0d3a299c8f
csiemichelin <csiemichelin@github.com>
2023-10-30 14:07:00 +0800

    10/30 mongoose mongoDB module

---
+++ Mongoose MongoDB module/137-mongoose連接數據庫.js
@@ -0,0 +1,27 @@
+// 1. 安裝 mongoose
+// 2. 導入 mongoose
+const mongoose = require('mongoose');
+
+// 3. 連接 mongodb服務                       數據庫名稱(若不存在則自動創建)                                    
+mongoose.connect('mongodb://127.0.0.1:27017/michelin');
+
+// 4. 設置回調函數
+// 設置連結成功的回調
+mongoose.connection.on('open', () => {
+    console.log('連接成功');
+});
+
+// 設置連結失敗的回調
+mongoose.connection.on('error', () => {
+    console.log('連接失敗');
+});
+
+// 設置連結關閉的回調
+mongoose.connection.on('close', () => {
+    console.log('連接關閉');
+});
+
+// 關閉 mongodb 服務的連接，2 sec Timeout
+setTimeout(() => {
+    mongoose.disconnect();
+}, 2000);
