e25fb9c9744b88d4e9b861c7b5154a8fa9ba0cf1
csiemichelin <csiemichelin@github.com>
2023-11-02 14:00:16 +0800

    11/2 案例實戰-記帳本結合數據庫 (151-155)

--- 案例實戰-記帳本結合數據庫 (151-155)/MichelinAccounts/routes/index.js
+++ 案例實戰-記帳本結合數據庫 (151-155)/MichelinAccounts/routes/index.js
@@ -7,41 +7,48 @@ const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync(__dirname + '/../data/db.json');
 // 獲取db 對象
 const db = low(adapter);
-// 導入shortid
-const shortid = require('shortid');
+// 導入momment
+const moment = require('moment');
+const AccountModel = require('../modules/AccountModel');
+// 測試
+console.log(moment('2023-02-24').toDate());
 
 /* GET home page. */
 // 記帳本列表表單
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   // 獲取所有的帳單訊息
   let accounts = db.get('accounts').value();
-  res.render('list', {accounts: accounts}); // 傳遞accounts參數
+  res.render('list', { accounts: accounts }); // 傳遞accounts參數
 });
 // 添加紀錄
-router.get('/create', function(req, res, next) {
+router.get('/create', function (req, res, next) {
   res.render('create');
 });
 // 新增紀錄
-router.post('/', function(req, res, next) {
-  // 獲取請求體數據
-  console.log(req.body);  // 參考 110-express獲取請求體body數據.js，但這邊不用添加中介函數，因為外層 app.js(#17 & #18)，已經把其設成全局中介函數了
-  // 生成id，用來區分每次存到db.json的資料
-  let id = shortid.generate();
-  // 寫入文件 (往前添加，讀取時可以較方便讀最近的)
-  db.get('accounts').unshift({id: id, ...req.body}).write();
-  // res.send('添加紀錄');
-  // 成功提醒
-  res.render('success', {msg: '添加成功喔~~~', url: '/account'});  // 傳msg和url參數進去
+router.post('/', function (req, res, next) {
+  // 獲取請求體數據 日期為字串 => new Date()
+  // 轉換步驟: "2023-02-24" => Object => new Date() // 透過momment包實現
+  // console.log(req.body);  // 參考 110-express獲取請求體body數據.js，但這邊不用添加中介函數，因為外層 app.js(#17 & #18)，已經把其設成全局中介函數了
+  AccountModel.create({
+    ...req.body,  //此語法代表其內容全部導入，案例實戰-記帳本 (122-129)/MichelinAccounts/route/index.js有用過
+    time: moment(req.body.time).toDate()  // 覆蓋原有的time屬性
+  }, (err, data) => {
+    if (err) {
+      res.status(500).send('插入失敗~~~');
+    }
+    // 成功提醒
+    res.render('success', { msg: '添加成功喔~~~', url: '/account' });  // 傳msg和url參數進去
+  });
 });
 // 刪除紀錄 (透過路由參數id的方式)
 router.get('/:id', (req, res) => {
   // 獲取路由參數id (參考100-express獲取路由參數.js)
   let id = req.params.id;
   // 刪除
-  db.get('accounts').remove({id: id}).write();
+  db.get('accounts').remove({ id: id }).write();
   // 提醒
   // res.send('刪除成功');
-  res.render('success', {msg: '刪除成功喔~~~', url: '/account'});  // 傳msg和url參數進去
+  res.render('success', { msg: '刪除成功喔~~~', url: '/account' });  // 傳msg和url參數進去
 });
 
 module.exports = router;
