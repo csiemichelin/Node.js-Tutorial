1cc846b9be90495b9a932db56bdef94fe45c44cd
B20626 <itriB20626@itri.org.tw>
2023-11-06 10:33:01 +0800

    11/6 express 會話控制

---
+++ 會話控制/171-express框架中設置cookie.js
@@ -0,0 +1,21 @@
+/**
+ * cookie通常是用戶登入時，伺服器會返回一個cookie給用戶
+ */
+
+// 導入express
+const express = require('express');
+
+// 創建應用對象
+const app = express();
+
+// 創建路由規則函數 (發送請求後，輸入http://127.0.0.1:3000，查看響應體有沒有cookie，請求體第一次發送不會有正確的cookie資訊)
+app.get('/set-cookie', (req, res) => {
+    // 1. 方法一: 會在瀏覽器關閉的時候，銷毀
+    res.cookie('name1', 'michelin1'); //第一個參數: 鍵名(cookie name), 第二個參數: 鍵值
+    // 2. 方法二: 對cookie設置生命週期
+    res.cookie('name2', 'michelin2', {maxAge: 60 * 1000}); //maxAge以毫秒為單位，範例為設置一分鐘    
+    res.send('home');
+});
+
+// 啟動服務
+app.listen(3000);
