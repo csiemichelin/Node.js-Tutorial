5078e0b6ceea48b45dd110dac442d29a32116550
B20626 <itriB20626@itri.org.tw>
2023-11-08 17:46:33 +0800

    11/8 案例實戰-記帳本註冊頁面 (179-186)

--- 案例實戰-記帳本註冊頁面 (179-186)/MichelinAccounts/routes/web/login.js
+++ 案例實戰-記帳本註冊頁面 (179-186)/MichelinAccounts/routes/web/login.js
@@ -4,12 +4,13 @@ var router = express.Router();
 const UserModel = require('../../modules/UserModel');
 const md5 = require('md5'); // 用來加密密碼(此為單向加密，無法將處理後的數據恢復為原始數據(無法解密))
 
-// 註冊
+// 註冊頁面
 router.get('/reg', (req, res) => {
     // 響應HTML內容
     res.render('login/reg');    // 透過模板引擎，參考115-express模板引擎ejs初體驗，因為在app.js設置全局中介函數，所以URL路徑為此
 });
 
+// 註冊操作
 router.post('/reg', (req, res) => {
     // 登入資訊驗證
     if (!req.body.username || typeof req.body.username !== 'string') {
@@ -23,11 +24,41 @@ router.post('/reg', (req, res) => {
     // console.log(req.body);
     UserModel.create({...req.body, password: md5(req.body.password)}, (err, data) => {
         if (err) {
-            res.status(500).send('註冊失敗，請稍後在試');
+            res.status(500).send('註冊失敗，請稍後在試~~~');
             return;
         } 
-        res.render('option/success', {msg: '註冊成功', url: '/login'});
+        res.render('option/success', {msg: '註冊成功', url: '/login'}); // 註冊好要網登錄頁面跳轉
     });
+});
+
+// 登錄頁面
+router.get('/login', (req, res) => {
+    // 響應HTML內容
+    res.render('login/login');    
+});
+
+// 登錄操作
+router.post('/login', (req, res) => {
+    // 獲取用戶名和密碼
+    let {username, password} = req.body;
+    // 查詢數據庫 (密碼數據庫中是加密過的，比對前要先將登錄密碼先做加密)
+    UserModel.findOne({username: username, password: md5(password)}, (err, data) => {
+        // 判斷
+        if (err) {
+            res.status(500).send('登錄失敗，請稍後在試~~~');
+            return;
+        } 
+        // 判斷data
+        // console.log(data);
+        if (!data) {
+            return res.send('帳號密碼錯誤~~~');
+        }
+        
+        // 登錄成功響應
+        res.render('option/success', {msg: '登錄成功', url: '../account/create'});
+    });
+    // 響應HTML內容
+    // res.render('login/login');    
 });
 
 module.exports = router;
